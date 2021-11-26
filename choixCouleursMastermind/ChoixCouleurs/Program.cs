8e5042d99c1dc521d31e1ece8a382a36fc367102
Michaux Dorian <190478@CCM-Asty-Moulin.local>
2021-11-26 10:57:50 +0100

    Changement des fonctions

--- choixCouleursMastermind/ChoixCouleurs/Program.cs
+++ choixCouleursMastermind/ChoixCouleurs/Program.cs
@@ -56,23 +56,71 @@ namespace choixCouleurs
                 if (colorsJoueur[i] == combi[i])
                 {
                     rouge = rouge + 1;
-                }
-                else
-                {
-                    rouge = 0;
+                    colorsJoueur[i] = "*";
+                    combi[i] = "#";
                 }
             }
             resultRouge = $"Languette(s) rouge : {rouge}";
+            
         }
 
-        public static void compareBlanc(string[] colorsJoueur, string[] combi)
+        public static void compareBlanc(string[] colorsJoueur, string[] combi, out int blanc, out string resultBlanc)
         {
-    
+
+            blanc = 0;
+
             for (int y = 0; y < 4; y++)
-            {
-               
+            {   
+                for(int n = 0; n < 4; n++)
+                {
+                    if(colorsJoueur[y] == combi[n])
+                    {
+                        blanc = blanc + 1;
+                        colorsJoueur[y] = "/";
+                    }
+                }
+
             }
-          
+            resultBlanc = $"Languette(s) blanche : {blanc}";
+        }
+
+        public static void cloneColorsJoueurArray(string[] colorsJoueur, out string[] cloneColorsJoueur)
+        {
+            cloneColorsJoueur = new string[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                cloneColorsJoueur[i] = colorsJoueur[i];
+            }
+
+        }
+
+        public static void cloneCombiArray(string[] combi, out string[] cloneCombi)
+        {
+            cloneCombi = new string[4];
+
+            for(int i = 0; i < 4; i++)
+            {
+                cloneCombi[i] = combi[i];
+            }
+        }
+
+
+       public static void affichageTableau(string[] colorsJoueur, string[] combi,out string joueur, out string com)
+        {
+            joueur = "";
+            com = "";
+            
+            for(int b = 0; b < 4; b++)
+            {
+                joueur = joueur + "-" +colorsJoueur[b];
+                com = com +"-"+ combi[b];
+
+            }
+
+            Console.WriteLine(joueur);
+            Console.WriteLine(com);
+
         }
 
        
@@ -84,12 +132,33 @@ namespace choixCouleurs
             string[] colorsJoueur = new string[4];
             string resultatColorsJoueur;
             string resultRouge;
-            int rouge; 
+            int rouge;
+            int blanc;
+            string resultBlanc;
+            string joueur;
+            string com;
+            string restart;
+            string[] cloneCombi;
+            string[] cloneColorsJoueur;
+            restart = "y";
+            
+            
             choixCouleurs(out colors, out combi);
-            afficher(combi);
-            couleursjoueurs(ref colorsJoueur, out resultatColorsJoueur);
-            CompareRouge(colorsJoueur, combi, out resultRouge, out rouge);
-            compareBlanc(colorsJoueur, combi);
+
+            while (restart == "y")
+            {
+                afficher(combi);
+                couleursjoueurs(ref colorsJoueur, out resultatColorsJoueur);
+                cloneColorsJoueurArray(colorsJoueur, out cloneColorsJoueur);
+                cloneCombiArray(combi, out cloneCombi);
+                CompareRouge(colorsJoueur, combi, out resultRouge, out rouge);
+                compareBlanc(colorsJoueur, combi, out blanc, out resultBlanc);
+                affichageTableau(colorsJoueur, combi, out joueur, out com);
+                Console.WriteLine(resultRouge);
+                Console.WriteLine(resultBlanc);
+                Console.WriteLine("Voulez vous rejouez ? y/n");
+                restart = Console.ReadLine();
+            }
             Console.ReadLine();
         }
     }
